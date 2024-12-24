4d88e138f03b2aeb356f0b064c2b438530a9fb6e
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-24 22:59:06 +1300

    (v766): [type] user_data_shapeless_recipe.rs

---
+++ crates/proto/src/version/v766/types/user_data_shapeless_recipe.rs
@@ -0,0 +1,20 @@
+use crate::version::v662::types::{NetworkItemInstanceDescriptor, RecipeIngredient};
+use bedrockrs_macros::ProtoCodec;
+use uuid::Uuid;
+use crate::version::v766::types::RecipeUnlockingRequirement;
+
+#[derive(ProtoCodec, Clone, Debug)]
+pub struct UserDataShapelessRecipe {
+    pub recipe_unique_id: String,
+    #[vec_repr(u32)]
+    #[vec_endianness(var)]
+    pub ingredient_list: Vec<RecipeIngredient>,
+    #[vec_repr(u32)]
+    #[vec_endianness(var)]
+    pub production_list: Vec<NetworkItemInstanceDescriptor>,
+    pub recipe_id: Uuid,
+    pub recipe_tag: String,
+    #[endianness(var)]
+    pub priority: i32,
+    pub unlocking_requirement: RecipeUnlockingRequirement,
+}
