d7aa6ec2c83080bcbbe34390105a2d4228d47f69
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-24 22:58:23 +1300

    (v766): [packet] container_registry_cleanup.rs

---
+++ crates/proto/src/version/v766/packets/container_registry_cleanup.rs
@@ -0,0 +1,9 @@
+use bedrockrs_macros::ProtoCodec;
+use crate::version::v766::types::FullContainerName;
+
+#[derive(ProtoCodec, Clone, Debug)]
+pub struct ContainerRegistryCleanupPacket {
+    #[vec_repr(u32)]
+    #[vec_endianness(var)]
+    pub removed_containers: Vec<FullContainerName>
+}
