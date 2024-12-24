4aab874251dffcf096a2290fb3fda77497eae7a5
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-24 22:58:28 +1300

    (v766): [packet] correct_player_move_prediction.rs

---
+++ crates/proto/src/version/v766/packets/correct_player_move_prediction.rs
@@ -0,0 +1,16 @@
+use crate::version::v662::enums::PredictionType;
+use bedrockrs_core::Vec3;
+use bedrockrs_macros::{gamepacket, ProtoCodec};
+
+#[gamepacket(id = 161)]
+#[derive(ProtoCodec, Clone, Debug)]
+pub struct CorrectPlayerMovePredictionPacket {
+    pub prediction_type: PredictionType,
+    #[endianness(le)]
+    pub position: Vec3<f32>,
+    #[endianness(le)]
+    pub velocity: Vec3<f32>,
+    pub on_ground: bool,
+    #[endianness(var)]
+    pub tick: u64,
+}
