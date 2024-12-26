05bac75d2e3cc6231a4bc2fb680603e66a37fb6d
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-27 12:24:55 +1300

    (v662): [enum] new_interaction_model.rs

--- crates/proto/src/version/v662/enums/new_interaction_model.rs
+++ crates/proto/src/version/v662/enums/new_interaction_model.rs
@@ -1,9 +1,9 @@
 use bedrockrs_macros::ProtoCodec;
 
 #[derive(ProtoCodec, Clone, Debug)]
-#[enum_repr(i32)]
+#[enum_repr(u32)]
 #[enum_endianness(var)]
-#[repr(i32)]
+#[repr(u32)]
 pub enum NewInteractionModel {
     Touch = 0,
     Crosshair = 1,
