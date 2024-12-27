9b7ac360a53ef4c630988d8439e3ff0156f8e3b5
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-27 21:52:06 +1300

    fix errors

--- crates/proto/src/version/v766/packets/player_auth_input.rs
+++ crates/proto/src/version/v766/packets/player_auth_input.rs
@@ -12,73 +12,75 @@ use bedrockrs_proto_core::{ProtoCodec, ProtoCodecLE, ProtoCodecVAR};
 use std::io::Cursor;
 use vek::{Vec2, Vec3};
 
-pub enum PlayerAuthInputFlags {
-    Ascend = 1 << 0,
-    Descend = 1 << 1,
+pub struct PlayerAuthInputFlags;
+
+impl PlayerAuthInputFlags {
+    pub const ASCEND: u128 = 1 << 0;
+    pub const DESCEND: u128 = 1 << 1;
     #[deprecated]
-    NorthJump = 1 << 2,
-    JumpDown = 1 << 3,
-    SprintDown = 1 << 4,
-    ChangeHeight = 1 << 5,
-    Jumping = 1 << 6,
-    AutoJumpingInWater = 1 << 7,
-    Sneaking = 1 << 8,
-    SneakDown = 1 << 9,
-    Up = 1 << 10,
-    Down = 1 << 11,
-    Left = 1 << 12,
-    Right = 1 << 13,
-    UpLeft = 1 << 14,
-    UpRight = 1 << 15,
-    WantUp = 1 << 16,
-    WantDown = 1 << 17,
-    WantDownSlow = 1 << 18,
-    WantUpSlow = 1 << 19,
-    Sprinting = 1 << 20,
-    AscendBlock = 1 << 21,
-    DescendBlock = 1 << 22,
-    SneakToggleDown = 1 << 23,
-    PersistSneak = 1 << 24,
-    StartSprinting = 1 << 25,
-    StopSprinting = 1 << 26,
-    StartSneaking = 1 << 27,
-    StopSneaking = 1 << 28,
-    StartSwimming = 1 << 29,
-    StopSwimming = 1 << 30,
-    StartJumping = 1 << 31,
-    StartGliding = 1 << 32,
-    StopGliding = 1 << 33,
-    PerformItemInteraction = 1 << 34,
-    PerformBlockActions = 1 << 35,
-    PerformItemStackRequest = 1 << 36,
-    HandleTeleport = 1 << 37,
-    Emoting = 1 << 38,
-    MissedSwing = 1 << 39,
-    StartCrawling = 1 << 40,
-    StopCrawling = 1 << 41,
-    StartFlying = 1 << 42,
-    StopFlying = 1 << 43,
-    ReceivedServerData = 1 << 44,
-    IsInClientPredictedVehicle = 1 << 45,
-    PaddleLeft = 1 << 46,
-    PaddleRight = 1 << 47,
-    BlockBreakingDelayEnabled = 1 << 48,
-    HorizontalCollision = 1 << 49,
-    VerticalCollision = 1 << 50,
-    DownLeft = 1 << 51,
-    DownRight = 1 << 52,
-    StartUsingItem = 1 << 53,
-    IsCameraRelativeMovementEnabled = 1 << 54,
-    IsRotControlledByMoveDirection = 1 << 55,
-    StartSpinAttack = 1 << 56,
-    StopSpinAttack = 1 << 57,
-    IsHotbarOnlyTouch = 1 << 58,
-    JumpReleasedRaw = 1 << 59,
-    JumpPressedRaw = 1 << 60,
-    JumpCurrentRaw = 1 << 61,
-    SneakReleasedRaw = 1 << 62,
-    SneakPressedRaw = 1 << 63,
-    SneakCurrentRaw = 1 << 64,
+    pub const NORTH_JUMP: u128 = 1 << 2;
+    pub const JUMP_DOWN: u128 = 1 << 3;
+    pub const SPRINT_DOWN: u128 = 1 << 4;
+    pub const CHANGE_HEIGHT: u128 = 1 << 5;
+    pub const JUMPING: u128 = 1 << 6;
+    pub const AUTO_JUMPING_IN_WATER: u128 = 1 << 7;
+    pub const SNEAKING: u128 = 1 << 8;
+    pub const SNEAK_DOWN: u128 = 1 << 9;
+    pub const UP: u128 = 1 << 10;
+    pub const DOWN: u128 = 1 << 11;
+    pub const LEFT: u128 = 1 << 12;
+    pub const RIGHT: u128 = 1 << 13;
+    pub const UP_LEFT: u128 = 1 << 14;
+    pub const UP_RIGHT: u128 = 1 << 15;
+    pub const WANT_UP: u128 = 1 << 16;
+    pub const WANT_DOWN: u128 = 1 << 17;
+    pub const WANT_DOWN_SLOW: u128 = 1 << 18;
+    pub const WANT_UP_SLOW: u128 = 1 << 19;
+    pub const SPRINTING: u128 = 1 << 20;
+    pub const ASCEND_BLOCK: u128 = 1 << 21;
+    pub const DESCEND_BLOCK: u128 = 1 << 22;
+    pub const SNEAK_TOGGLE_DOWN: u128 = 1 << 23;
+    pub const PERSIST_SNEAK: u128 = 1 << 24;
+    pub const START_SPRINTING: u128 = 1 << 25;
+    pub const STOP_SPRINTING: u128 = 1 << 26;
+    pub const START_SNEAKING: u128 = 1 << 27;
+    pub const STOP_SNEAKING: u128 = 1 << 28;
+    pub const START_SWIMMING: u128 = 1 << 29;
+    pub const STOP_SWIMMING: u128 = 1 << 30;
+    pub const START_JUMPING: u128 = 1 << 31;
+    pub const START_GLIDING: u128 = 1 << 32;
+    pub const STOP_GLIDING: u128 = 1 << 33;
+    pub const PERFORM_ITEM_INTERACTION: u128 = 1 << 34;
+    pub const PERFORM_BLOCK_ACTIONS: u128 = 1 << 35;
+    pub const PERFORM_ITEM_STACK_REQUEST: u128 = 1 << 36;
+    pub const HANDLE_TELEPORT: u128 = 1 << 37;
+    pub const EMOTING: u128 = 1 << 38;
+    pub const MISSED_SWING: u128 = 1 << 39;
+    pub const START_CRAWLING: u128 = 1 << 40;
+    pub const STOP_CRAWLING: u128 = 1 << 41;
+    pub const START_FLYING: u128 = 1 << 42;
+    pub const STOP_FLYING: u128 = 1 << 43;
+    pub const RECEIVED_SERVER_DATA: u128 = 1 << 44;
+    pub const IS_IN_CLIENT_PREDICTED_VEHICLE: u128 = 1 << 45;
+    pub const PADDLE_LEFT: u128 = 1 << 46;
+    pub const PADDLE_RIGHT: u128 = 1 << 47;
+    pub const BLOCK_BREAKING_DELAY_ENABLED: u128 = 1 << 48;
+    pub const HORIZONTAL_COLLISION: u128 = 1 << 49;
+    pub const VERTICAL_COLLISION: u128 = 1 << 50;
+    pub const DOWN_LEFT: u128 = 1 << 51;
+    pub const DOWN_RIGHT: u128 = 1 << 52;
+    pub const START_USING_ITEM: u128 = 1 << 53;
+    pub const IS_CAMERA_RELATIVE_MOVEMENT_ENABLED: u128 = 1 << 54;
+    pub const IS_ROT_CONTROLLED_BY_MOVE_DIRECTION: u128 = 1 << 55;
+    pub const START_SPIN_ATTACK: u128 = 1 << 56;
+    pub const STOP_SPIN_ATTACK: u128 = 1 << 57;
+    pub const IS_HOTBAR_ONLY_TOUCH: u128 = 1 << 58;
+    pub const JUMP_RELEASED_RAW: u128 = 1 << 59;
+    pub const JUMP_PRESSED_RAW: u128 = 1 << 60;
+    pub const JUMP_CURRENT_RAW: u128 = 1 << 61;
+    pub const SNEAK_RELEASED_RAW: u128 = 1 << 62;
+    pub const SNEAK_PRESSED_RAW: u128 = 1 << 63;
+    pub const SNEAK_CURRENT_RAW: u128 = 1 << 64;
 }
 
 #[derive(ProtoCodec, Clone, Debug)]
@@ -145,25 +147,25 @@ impl ProtoCodec for PlayerAuthInputPacket {
         <Vec3<f32> as ProtoCodecLE>::proto_serialize(&self.interact_rotation, stream)?;
         <u64 as ProtoCodecVAR>::proto_serialize(&self.client_tick, stream)?;
         <Vec3<f32> as ProtoCodecLE>::proto_serialize(&self.velocity, stream)?;
-        if &self.input_data & PlayerAuthInputFlags::PerformItemInteraction as u128 != 0 {
+        if &self.input_data & PlayerAuthInputFlags::PERFORM_ITEM_INTERACTION != 0 {
             <PackedItemUseLegacyInventoryTransaction as ProtoCodec>::proto_serialize(
                 &self.item_use_transaction.as_ref().unwrap(),
                 stream,
             )?;
         }
-        if &self.input_data & PlayerAuthInputFlags::PerformItemStackRequest as u128 != 0 {
+        if &self.input_data & PlayerAuthInputFlags::PERFORM_ITEM_STACK_REQUEST != 0 {
             <PerformItemStackRequestData as ProtoCodec>::proto_serialize(
                 &self.item_stack_request.as_ref().unwrap(),
                 stream,
             )?;
         }
-        if &self.input_data & PlayerAuthInputFlags::PerformBlockActions as u128 != 0 {
+        if &self.input_data & PlayerAuthInputFlags::PERFORM_BLOCK_ACTIONS != 0 {
             <PlayerBlockActions as ProtoCodec>::proto_serialize(
                 &self.player_block_actions.as_ref().unwrap(),
                 stream,
             )?;
         }
-        if &self.input_data & PlayerAuthInputFlags::IsInClientPredictedVehicle as u128 != 0 {
+        if &self.input_data & PlayerAuthInputFlags::IS_IN_CLIENT_PREDICTED_VEHICLE != 0 {
             <ClientPredictedVehicleData as ProtoCodec>::proto_serialize(
                 &self.client_predicted_vehicle.as_ref().unwrap(),
                 stream,
@@ -189,7 +191,7 @@ impl ProtoCodec for PlayerAuthInputPacket {
         let client_tick = <u64 as ProtoCodecVAR>::proto_deserialize(stream)?;
         let velocity = <Vec3<f32> as ProtoCodecLE>::proto_deserialize(stream)?;
         let item_use_transaction = match &input_data
-            & PlayerAuthInputFlags::PerformItemInteraction as u128
+            & PlayerAuthInputFlags::PERFORM_ITEM_INTERACTION
             != 0
         {
             true => Some(
@@ -198,21 +200,21 @@ impl ProtoCodec for PlayerAuthInputPacket {
             false => None,
         };
         let item_stack_request = match &input_data
-            & PlayerAuthInputFlags::PerformItemStackRequest as u128
+            & PlayerAuthInputFlags::PERFORM_ITEM_STACK_REQUEST
             != 0
         {
             true => Some(<PerformItemStackRequestData as ProtoCodec>::proto_deserialize(stream)?),
             false => None,
         };
         let player_block_actions =
-            match &input_data & PlayerAuthInputFlags::PerformBlockActions as u128 != 0 {
+            match &input_data & PlayerAuthInputFlags::PERFORM_BLOCK_ACTIONS != 0 {
                 true => Some(<PlayerBlockActions as ProtoCodec>::proto_deserialize(
                     stream,
                 )?),
                 false => None,
             };
         let client_predicted_vehicle = match &input_data
-            & PlayerAuthInputFlags::IsInClientPredictedVehicle as u128
+            & PlayerAuthInputFlags::IS_IN_CLIENT_PREDICTED_VEHICLE
             != 0
         {
             true => Some(<ClientPredictedVehicleData as ProtoCodec>::proto_deserialize(stream)?),
@@ -256,19 +258,19 @@ impl ProtoCodec for PlayerAuthInputPacket {
             + ProtoCodecLE::get_size_prediction(&self.interact_rotation)
             + ProtoCodecVAR::get_size_prediction(&self.client_tick)
             + ProtoCodecLE::get_size_prediction(&self.velocity)
-            + match &self.input_data & PlayerAuthInputFlags::PerformItemInteraction as u128 != 0 {
+            + match &self.input_data & PlayerAuthInputFlags::PERFORM_ITEM_INTERACTION != 0 {
                 true => self.item_use_transaction.get_size_prediction(),
                 false => 0,
             }
-            + match &self.input_data & PlayerAuthInputFlags::PerformItemStackRequest as u128 != 0 {
+            + match &self.input_data & PlayerAuthInputFlags::PERFORM_ITEM_STACK_REQUEST != 0 {
                 true => self.item_stack_request.get_size_prediction(),
                 false => 0,
             }
-            + match &self.input_data & PlayerAuthInputFlags::PerformBlockActions as u128 != 0 {
+            + match &self.input_data & PlayerAuthInputFlags::PERFORM_BLOCK_ACTIONS != 0 {
                 true => self.player_block_actions.get_size_prediction(),
                 false => 0,
             }
-            + match &self.input_data & PlayerAuthInputFlags::IsInClientPredictedVehicle as u128 != 0
+            + match &self.input_data & PlayerAuthInputFlags::IS_IN_CLIENT_PREDICTED_VEHICLE != 0
             {
                 true => self.client_predicted_vehicle.get_size_prediction(),
                 false => 0,
