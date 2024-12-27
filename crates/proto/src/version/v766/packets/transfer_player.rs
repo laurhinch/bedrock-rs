9877c043c1e8c2c58812bd62fb22bc8e29dcba95
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-27 21:22:11 +1300

    (v766): [packet] transfer_player.rs

---
+++ crates/proto/src/version/v766/packets/transfer_player.rs
@@ -0,0 +1,10 @@
+use bedrockrs_macros::{gamepacket, ProtoCodec};
+
+#[gamepacket(id = 85)]
+#[derive(ProtoCodec, Clone, Debug)]
+pub struct TransferPlayerPacket {
+    pub server_address: String,
+    #[endianness(le)]
+    pub server_port: u16,
+    pub reload_world: bool,
+}
