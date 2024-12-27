5a9bd275af85775c16f6bfd9c3fc0f3b3184f9ac
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-27 21:21:44 +1300

    (v766): [packet] server_bound_loading_screen.rs

---
+++ crates/proto/src/version/v766/packets/server_bound_loading_screen.rs
@@ -0,0 +1,18 @@
+use bedrockrs_macros::ProtoCodec;
+
+#[derive(ProtoCodec, Clone, Debug)]
+#[enum_repr(i32)]
+#[enum_endianness(var)]
+#[repr(i32)]
+pub enum LoadingScreenType {
+    Unknown = 0,
+    StartLoadingScreen = 1,
+    EndLoadingScreen = 2,
+}
+
+#[derive(ProtoCodec, Clone, Debug)]
+pub struct ServerBoundLoadingScreenPacket {
+    pub loading_screen_type: LoadingScreenType,
+    #[endianness(var)]
+    pub loading_screen_id: Option<u32>,
+}
