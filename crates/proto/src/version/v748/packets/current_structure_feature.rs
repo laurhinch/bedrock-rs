2d14accf809aec8d2daeb1518d2e1ae8feb9efd1
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2024-12-31 13:19:43 +1300

    packet id

--- crates/proto/src/version/v748/packets/current_structure_feature.rs
+++ crates/proto/src/version/v748/packets/current_structure_feature.rs
@@ -1,5 +1,6 @@
-use bedrockrs_macros::ProtoCodec;
+use bedrockrs_macros::{gamepacket, ProtoCodec};
 
+#[gamepacket(id = 314)]
 #[derive(ProtoCodec, Clone, Debug)]
 pub struct CurrentStructureFeaturePacket {
     pub current_structure_feature: String,
